a25999343e002d7b77093bb8800267c12cca2eaf
ZAIN MAHAR <144410914+zainMahar78@users.noreply.github.com>
2024-02-01 11:38:50 -0800

    Add files via upload

---
+++ Credentials.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bi3
+{
+    internal class Credentials
+    {
+       public string name;
+       public string password;
+       public string role;
+        public Credentials(string Aname, string Apassword, string Arole)
+        {
+           name = Aname;
+            password = Apassword;
+            role = Arole;
+        }
+    }
+}
---
+++ Products.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bi3
+{
+    internal class Products
+    {
+        public string productname;
+        public float productprice;
+        public int productquantity;
+        public Products(string itemName, float price, int quantity) {
+            productname = itemName;
+            productprice = price;
+            productquantity = quantity;
+        }
+    }
+}
